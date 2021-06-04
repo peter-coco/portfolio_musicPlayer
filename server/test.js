d9ebe220930638eb820eef9746a67116c0581455
peter <dklirser@gmail.com>
2021-06-04 18:31:21 +0900

    port test

--- server/test.js
+++ server/test.js
@@ -1,6 +1,6 @@
 const http = require("http");
 
-const hostname = "127.0.0.1"; // 서버 컴퓨터의 ip
+const hostname = "0.0.0.0"; // 서버 컴퓨터의 ip
 const port = 8080; //
 
 const server = http.createServer((req, res) => {
