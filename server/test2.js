090fd8ed9b0efad3541058ac139af868de7c7ed6
peter <dklirser@gmail.com>
2021-06-05 00:39:22 +0900

    port test

---
+++ server/test2.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const vhost = require("vhost");
+
+const app1 = express();
+const app = express();
+
+app1.get("/", function (req, res) {
+  res.send("app1");
+});
+
+app.use(vhost("https://musicdata.link/", app1));
+app.listen(3000, () => {
+  console.log("start");
+});
---
+++ server/test3.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const vhost = require("vhost");
+
+const app1 = express();
+const app = express();
+
+app1.get("/", function (req, res) {
+  res.send("app1");
+});
+
+app.use(vhost("https://musicdata.link/", app1));
+
+app.listen(443, () => {
+  console.log("start");
+});
