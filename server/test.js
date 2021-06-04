2c3cb0e42ab0600a3ce6f93ee5323e600ec229e6
peter <dklirser@gmail.com>
2021-06-05 00:13:03 +0900

    port test

--- server/test.js
+++ server/test.js
@@ -1,17 +1,11 @@
-const https = require("https");
+var express = require("express"),
+  site1 = express.createServer(),
+  site_vhosts = [],
+  vhost;
 
-const hostname = "54.180.99.34"; // 서버 컴퓨터의 ip
-const port = 8080; //
+site_vhosts.push(express.vhost("https://musicdata.link/", site1));
 
-const server = https.createServer((req, res) => {
-  // createServer 명령을 통해 서버 한대를 만든다.
-  res.statusCode = 200; // 통신 성공
-  res.setHeader("Content-Type", "text/plain");
-  res.end("Hello World\n");
-});
+vhost = express.createServer.apply(this, site_vhosts);
 
-server.listen(port, hostname, () => {
-  // 만든 서버가 이 컴퓨터에 리스닝을 하도록 시킨다.
-  // 첫번째 인자 port는 3000번이고 hostname은 이 컴퓨터의 ip 같은 것
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+site1.listen(8080);
+vhost.listen(80);
