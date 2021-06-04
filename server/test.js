b3f5954d68af4b9a63272139aebbaa81d0c35258
peter <dklirser@gmail.com>
2021-06-05 00:14:55 +0900

    port test

--- server/test.js
+++ server/test.js
@@ -1,11 +1,11 @@
-var express = require("express"),
-  site1 = express.createServer(),
-  site_vhosts = [],
-  vhost;
+const express = require("express");
+const site1 = express.createServer();
+const site_vhosts = [];
+const vhost;
 
 site_vhosts.push(express.vhost("https://musicdata.link/", site1));
 
-vhost = express.createServer.apply(this, site_vhosts);
+vhost = express.createServer().apply(this, site_vhosts);
 
 site1.listen(8080);
 vhost.listen(80);
