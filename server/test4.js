8c6a62787f9166eb597a9250eda52729af177bd5
peter <dklirser@gmail.com>
2021-06-06 10:06:47 +0900

    server test

--- server/test4.js
+++ server/test4.js
@@ -4,35 +4,35 @@ const http = require("http");
 const fs = require("fs");
 const PORT = process.env.PORT || 443; // HTTPS 는 443 포트를 사용합니다
 
-const option =
-  process.env.NODE_ENV === "production"
-    ? {
-        key: fs.readFileSync(
-          __dirname + "/etc/letsencrypt/live/musicdata.link/privkey.pem"
-        ),
-        cert: fs.readFileSync(
-          __dirname + "/etc/letsencrypt/live/musicdata.link/cert.pem"
-        ),
-        ca: fs.readFileSync(
-          __dirname + "/etc/letsencrypt/live/musicdata.link/fullchain.pem"
-        ),
-      }
-    : undefined;
+// const option =
+//   process.env.NODE_ENV === "production"
+//     ? {
+//         key: fs.readFileSync(
+//           __dirname + "/etc/letsencrypt/live/musicdata.link/privkey.pem"
+//         ),
+//         cert: fs.readFileSync(
+//           __dirname + "/etc/letsencrypt/live/musicdata.link/cert.pem"
+//         ),
+//         ca: fs.readFileSync(
+//           __dirname + "/etc/letsencrypt/live/musicdata.link/fullchain.pem"
+//         ),
+//       }
+//     : undefined;
 
-// production 모드에서는 https 서버를
-// development 모드에서는 http 서버를 사용합니다
-option
-  ? https.createServer(option, app).listen(PORT, () => {
-      console.log(`Server is running at port ${PORT}`);
-    })
-  : http.createServer(app).listen(PORT, () => {
-      console.log(`Server is running at port ${PORT}`);
-    });
+// // production 모드에서는 https 서버를
+// // development 모드에서는 http 서버를 사용합니다
+// option
+//   ? https.createServer(option, app).listen(PORT, () => {
+//       console.log(`Server is running at port ${PORT}`);
+//     })
+//   : http.createServer(app).listen(PORT, () => {
+//       console.log(`Server is running at port ${PORT}`);
+//     });
 
-// const option = http.createServer(app).listen(80, () => {
-//   // console.log(`Server is running at port ${80}`);
-//   console.log("server is running");
-// });
+const option = http.createServer(app).listen(80, () => {
+  // console.log(`Server is running at port ${80}`);
+  console.log("server is running");
+});
 
 app.get("/address", (req, res) => {
   // res.header({
