491958c8005aef533424425be101791d6b22e4c7
peter <dklirser@gmail.com>
2021-06-06 09:44:31 +0900

    server test

--- server/test4.js
+++ server/test4.js
@@ -4,20 +4,20 @@ const http = require("http");
 const fs = require("fs");
 const PORT = process.env.PORT || 443; // HTTPS 는 443 포트를 사용합니다
 
-const option =
-  process.env.NODE_ENV === "production"
-    ? {
-        key: fs.readFileSync(
-          __dirname + "/etc/letsencrypt/live/musicdata.link/privkey.pem"
-        ),
-        cert: fs.readFileSync(
-          __dirname + "/etc/letsencrypt/live/musicdata.link/cert.pem"
-        ),
-        ca: fs.readFileSync(
-          __dirname + "/etc/letsencrypt/live/musicdata.link/fullchain.pem"
-        ),
-      }
-    : undefined;
+// const option =
+//   process.env.NODE_ENV === "production"
+//     ? {
+//         key: fs.readFileSync(
+//           __dirname + "/etc/letsencrypt/live/musicdata.link/privkey.pem"
+//         ),
+//         cert: fs.readFileSync(
+//           __dirname + "/etc/letsencrypt/live/musicdata.link/cert.pem"
+//         ),
+//         ca: fs.readFileSync(
+//           __dirname + "/etc/letsencrypt/live/musicdata.link/fullchain.pem"
+//         ),
+//       }
+//     : undefined;
 
 // production 모드에서는 https 서버를
 // development 모드에서는 http 서버를 사용합니다
@@ -29,7 +29,7 @@ const option =
 //       console.log(`Server is running at port ${PORT}`);
 //     });
 
-option = http.createServer(app).listen(PORT, () => {
+const option = http.createServer(app).listen(PORT, () => {
   console.log(`Server is running at port ${PORT}`);
 });
 
