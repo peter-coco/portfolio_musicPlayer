15b7cc8a6d9dc4deeb4c05664f832ea839f6f63e
peter <dklirser@gmail.com>
2021-06-06 11:32:24 +0900

    server img

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const app = express();
 const cors = require("cors");
 const http = require("http");
 
-const img1 = require("./albums/Bad.png");
+// const img1 = require("./albums/Bad.png");
 const { fstat } = require("node:fs");
 // import img1 from "./albums/Bad.png";
 // import img1 from "./albums/Bad.png";
